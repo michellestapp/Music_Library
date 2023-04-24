ee47b3ee2cc1f07655431ce227a3db39de6cc55f
michellestapp <mstapp@rocketmail.com>
2023-04-24 16:06:45 -0500

    Added Delete, MVP put function for update

--- app.py
+++ app.py
@@ -75,7 +75,29 @@ class SongListResources(Resource):
 class SongResources(Resource):
     def get(self,song_id):
         song_from_db = Song.query.get_or_404(song_id)
-        return song_schema.dump(song_from_db), 
+        return song_schema.dump(song_from_db), 200
+    
+    def put(self, song_id):
+        song_from_db = Song.query.get_or_404(song_id)
+
+        if 'title' in request.json:
+            song_from_db.title = request.json['title']
+        if 'artist' in request.json:
+            song_from_db.artist = request.json['artist']
+        if 'album' in request.json:
+            song_from_db.album = request.json['album']
+        if 'release_date' in request.json:
+            song_from_db.release_date = request.json['release_date']
+        if 'genre' in request.json:
+            song_from_db.genre = request.json['genre']
+
+        db.session.commit()
+        return song_schema.dump(song_id), 200
+    
+    def delete(self,song_id):
+        song_from_db = Song.query.get_or_404(song_id)
+        db.session.delete(song_from_db)
+        return '',204
 
 # Routes
 
